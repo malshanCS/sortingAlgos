10ec6ee1d2e8787c14c7851221bd0a517497d1d7
malshanCS <105127660+malshanCS@users.noreply.github.com>
2022-05-10 23:19:01 +0530

    Create optimizedBubbleSort.java

---
+++ Java/optimizedBubbleSort.java
@@ -0,0 +1,32 @@
+public class bubbleSort {  
+  public void sortOptimized(int[] array) {
+        int lengthOfArray = array.length;
+
+        for (int j = 1; (j < lengthOfArray - 1); j++) {
+
+            // To check whether no swapping has done through one iteration of above outer
+            // loop
+            int flag = 0;
+
+            for (int i = 0; i < (lengthOfArray - j); i++) {
+
+                if (array[i] > array[i + 1]) {
+
+                    // Swap function
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+
+                    // atleast Swap has done, Set flag = 1
+                    flag = 1;
+                }
+            }
+            // No swap has done, break the outer loop
+            if (flag == 0) {
+                break;
+            }
+        }
+
+    }
+
+}
